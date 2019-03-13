78b1485a6c96976a0e9d140e85906e7b97752e1f
rokatyy <erogatova@gmail.com>
2019-03-13 12:45:38 +0300

    first fix

--- example.py
+++ example.py
@@ -10,6 +10,6 @@ TEMPLATE_TEXT = "Hello [% thing %]!"
 t = Template()
 
 try:
-    print t.processString(TEMPLATE_TEXT, { "thing": "world" })
-except TemplateException, e:
-    print "ERROR: %s" % e
+    print(t.processString(TEMPLATE_TEXT, { "thing": "world" }))
+except TemplateException as e:
+    print("ERROR: %s" % e)
